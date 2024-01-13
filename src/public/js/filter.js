a020764f6a179e7bc9e616cfc15b6d9d56ea8d11
VŨ ĐỨC HOÀNG <Hoangbarca1492@gmail.com>
2024-01-13 21:23:46 +0700

    update font

--- src/public/js/filter.js
+++ src/public/js/filter.js
@@ -12,8 +12,21 @@ let currentPage = 1
 function getBook(type, param, page) {
     axios.get(`${bookUrl}?q=${type}:${param}&startIndex=${(page - 1) * 9}&maxResults=9`)
         .then(res => {
-            console.log(res);
+            console.log(res.data);
+
             let fragment = new DocumentFragment();
+            if (res.data.totalItems == 0) {
+                const div = document.createElement('div')
+                div.innerHTML = `<i class="fa-solid fa-face-tired"></i>
+                    <span>
+                NOT FOUND
+                </span> `
+                div.classList.add('not-found')
+                fragment.appendChild(div)
+                listBook.removeChild(loading)
+                listBook.appendChild(fragment)
+                return
+            }
             res.data.items.forEach(book => {
                 const card = makeBookCard(book)
                 fragment.appendChild(card)
